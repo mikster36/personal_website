ba8ed3b0ca57467073a29c565c5b0d79b00c5b84
michael <45michaelo88@gmail.com>
2022-12-16 00:24:13 -0500

    Added more info for PDD card and removed unused file

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 filterSelection('all');
 function filterSelection(c) {
     let x, i;
-    x = document.getElementsByClassName("filterDiv");
+    x = document.getElementsByClassName("card");
     if (c === "all") c = "";
     // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
     for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
+        removeClass(x[i], "show");
         if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
     }
 }
 
 // Show filtered elements
-function w3AddClass(element, name) {
+function addClass(element, name) {
     let i, arr1, arr2;
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
@@ -23,7 +23,7 @@ function w3AddClass(element, name) {
 }
 
 // Hide elements that are not selected
-function w3RemoveClass(element, name) {
+function removeClass(element, name) {
     let i, arr1, arr2;
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
@@ -36,8 +36,8 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-const btnContainer = document.getElementById("myBtnContainer");
-const btns = btnContainer.getElementsByClassName("btn");
+const btnContainer = document.getElementById("btn group");
+const btns = btnContainer.getElementsByClassName("filter");
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function() {
         let current = document.getElementsByClassName("active");
